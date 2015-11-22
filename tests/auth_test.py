86ca97aa4f4561f0cbefe4fd5bfb6372120d4b9e
nanomishka <mihail777str@yandex.ru>
2015-11-22 22:01:20 +0300

    start pageOpject

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -6,31 +6,7 @@ import urlparse
 from selenium.webdriver import DesiredCapabilities, Remote
 from selenium.webdriver.support.ui import WebDriverWait
 
-from base import Page, Component
-
-
-class AuthPage(Page):
-    PATH = ''
-
-    @property
-    def form(self):
-        return AuthForm(self.driver)
-
-
-class AuthForm(Component):
-    LOGIN = '//input[@name="Login"]'
-    PASSWORD = '//input[@name="Password"]'
-    SUBMIT = '//button[contains(concat(" ", @class, " "), " js-login-page__external__submit ")]'
-
-    def set_login(self, login):
-        self.driver.find_element_by_xpath(self.LOGIN).send_keys(login)
-
-    def set_password(self, pwd):
-        self.driver.find_element_by_xpath(self.PASSWORD).send_keys(pwd)
-
-    def submit(self):
-        self.driver.find_element_by_xpath(self.SUBMIT).click()
-
+from base import *
 
 class AuthTest(unittest.TestCase):
     USERNAME = u'testuser'
@@ -44,18 +20,24 @@ class AuthTest(unittest.TestCase):
             command_executor='http://127.0.0.1:4444/wd/hub',
             desired_capabilities=getattr(DesiredCapabilities, browser).copy()
         )
-
-    def tearDown(self):
-        self.driver.quit()
-
-    def test(self):
-        auth_page = AuthPage(self.driver)
-        auth_page.open()
-
         auth_page = AuthPage(self.driver)
         auth_page.open()
         auth_form = auth_page.form
         auth_form.set_login(self.USER_EMAIL)
         auth_form.set_password(self.PASSWORD)
         auth_form.submit()
+
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test(self):
+        inbox_page = InboxPage(self.driver)
+        email = inbox_page.top_status.get_email()
+
+        self.assertEquals(email, self.USER_EMAIL)
+        
+
+
+        
 
--- tests/base.py
+++ tests/base.py
@@ -8,7 +8,6 @@ import urlparse
 from selenium.webdriver import DesiredCapabilities, Remote
 from selenium.webdriver.support.ui import WebDriverWait
 
-
 class Page(object):
     BASE_URL = 'https://e.mail.ru/'
     PATH = ''
@@ -21,9 +20,40 @@ class Page(object):
         self.driver.get(url)
         self.driver.maximize_window()
 
-
 class Component(object):
     def __init__(self, driver):
         self.driver = driver
 
+class AuthPage(Page):
+    PATH = ''
 
+    @property
+    def form(self):
+        return AuthForm(self.driver)
+
+class InboxPage(Page):
+    PATH = ''
+
+    @property
+    def top_status(self):
+        return TopStatus(self.driver)
+
+class AuthForm(Component):
+    LOGIN = '//input[@name="Login"]'
+    PASSWORD = '//input[@name="Password"]'
+    SUBMIT = '//button[contains(concat(" ", @class, " "), " js-login-page__external__submit ")]'
+
+    def set_login(self, login):
+        self.driver.find_element_by_xpath(self.LOGIN).send_keys(login)
+
+    def set_password(self, pwd):
+        self.driver.find_element_by_xpath(self.PASSWORD).send_keys(pwd)
+
+    def submit(self):
+        self.driver.find_element_by_xpath(self.SUBMIT).click()
+
+class TopStatus(Component):
+    EMAIL = '//i[@id="PH_user-email"]'
+
+    def get_email(self):
+        return self.driver.find_element_by_xpath(self.EMAIL).text
